326c71455744b71f56c25a619b85ff023ae1fa2d
Mubashir Hasan <mubashirhasan716@gmail.com>
2021-03-28 19:25:18 +0530

    first commit

---
+++ dist/app.js
@@ -0,0 +1,65 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+app.get("/", function (req, res) {
+    res.send("Hello Eamar User!");
+});
+var SHA256 = require("crypto-js/sha256");
+var CryptoBlock = /** @class */ (function () {
+    function CryptoBlock(index, timestamp, data, precedingHash) {
+        if (precedingHash === void 0) { precedingHash = " "; }
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.precedingHash = precedingHash;
+        this.hash = this.computeHash();
+    }
+    CryptoBlock.prototype.computeHash = function () {
+        return SHA256(this.index +
+            this.precedingHash +
+            this.timestamp +
+            JSON.stringify(this.data)).toString();
+    };
+    return CryptoBlock;
+}());
+var CryptoBlockchain = /** @class */ (function () {
+    function CryptoBlockchain() {
+        this.blockchain = [this.startGenesisBlock()];
+    }
+    CryptoBlockchain.prototype.startGenesisBlock = function () {
+        return new CryptoBlock(0, "28/03/2021", "Initial Block in the Chain", "0");
+    };
+    CryptoBlockchain.prototype.obtainLatestBlock = function () {
+        return this.blockchain[this.blockchain.length - 1];
+    };
+    CryptoBlockchain.prototype.addNewBlock = function (newBlock) {
+        newBlock.precedingHash = this.obtainLatestBlock().hash;
+        newBlock.hash = newBlock.computeHash();
+        this.blockchain.push(newBlock);
+    };
+    return CryptoBlockchain;
+}());
+var smashingCoin = new CryptoBlockchain();
+smashingCoin.addNewBlock(new CryptoBlock(1, "27/3/2021", {
+    sender: "Iris Ljesnjanin",
+    recipient: "Cosima Mielke",
+    quantity: 50,
+}));
+smashingCoin.addNewBlock(new CryptoBlock(2, "28/03/2021", {
+    sender: "Vitaly Friedman",
+    recipient: "Ricardo Gimenes",
+    quantity: 100,
+}));
+smashingCoin.addNewBlock(new CryptoBlock(3, "28/03/2021", {
+    sender: "Mohammed Mubashir Hasan",
+    recipient: "Donald Trump",
+    quantity: 1000000,
+}));
+console.log(JSON.stringify(smashingCoin, null, 4));
+app.listen(5000, function (req, res) {
+    console.log("listening on port 5000");
+});
---
+++ src/app.ts
@@ -0,0 +1,103 @@
+import express from "express";
+
+interface Block {
+  index: number;
+  timestamp: Date;
+  data: any;
+  precedingHash: string;
+}
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Eamar User!");
+});
+
+const SHA256 = require("crypto-js/sha256");
+class CryptoBlock {
+  index: number;
+  timestamp: any;
+  data: any;
+  precedingHash: any;
+  hash: string;
+  constructor(
+    index: number,
+    timestamp: any,
+    data: any,
+    precedingHash: any = " "
+  ) {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.precedingHash = precedingHash;
+    this.hash = this.computeHash();
+  }
+  computeHash() {
+    return SHA256(
+      this.index +
+        this.precedingHash +
+        this.timestamp +
+        JSON.stringify(this.data)
+    ).toString();
+  }
+}
+class CryptoBlockchain {
+  blockchain: any;
+  constructor() {
+    this.blockchain = [this.startGenesisBlock()];
+  }
+  startGenesisBlock() {
+    return new CryptoBlock(0, "28/03/2021", "Initial Block in the Chain", "0");
+  }
+  obtainLatestBlock() {
+    return this.blockchain[this.blockchain.length - 1];
+  }
+  addNewBlock(newBlock: any) {
+    newBlock.precedingHash = this.obtainLatestBlock().hash;
+    newBlock.hash = newBlock.computeHash();
+    this.blockchain.push(newBlock);
+  }
+  checkChainValidity() {
+    for (let i = 1; i < this.blockchain.length; i++) {
+      const currentBlock = this.blockchain[i];
+      const precedingBlock = this.blockchain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.computeHash()) {
+        return false;
+        console.log(
+          "This blockchain has been tampered with. It is now modified or corrupted"
+        );
+      }
+      if (currentBlock.precedingHash !== precedingBlock.hash) return false;
+    }
+    return true;
+  }
+}
+
+let smashingCoin = new CryptoBlockchain();
+smashingCoin.addNewBlock(
+  new CryptoBlock(1, "27/3/2021", {
+    sender: "Iris Ljesnjanin",
+    recipient: "Cosima Mielke",
+    quantity: 50,
+  })
+);
+smashingCoin.addNewBlock(
+  new CryptoBlock(2, "28/03/2021", {
+    sender: "Vitaly Friedman",
+    recipient: "Ricardo Gimenes",
+    quantity: 100,
+  })
+);
+smashingCoin.addNewBlock(
+  new CryptoBlock(3, "28/03/2021", {
+    sender: "Mohammed Mubashir Hasan",
+    recipient: "Donald Trump",
+    quantity: 1000000,
+  })
+);
+console.log(JSON.stringify(smashingCoin, null, 4));
+
+app.listen(5000, (req, res) => {
+  console.log("listening on port 5000");
+});
